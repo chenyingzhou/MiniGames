b6cbbfbc7493c6517322d987533e32a5255df06e
chenyingzhou <echocyz@gmail.com>
2018-09-12 16:48:41 +0800

    分享按钮

--- OneLine/src/widget/ButtonSet.ts
+++ OneLine/src/widget/ButtonSet.ts
@@ -4,6 +4,7 @@ namespace ButtonSet {
         protected buttons = {
             "start": new Button(Button.START),
             "rank": new Button(Button.RANK),
+            "share": new Button(Button.SHARE),
         };
 
         public static getInstance(): ButtonSet {
@@ -26,10 +27,13 @@ namespace ButtonSet {
 
         public loadCommon() {
             this.scaleX = this.scaleY = 0.8;
-            this.buttons.rank.x = this.width / 3;
+            this.buttons.share.x = this.width / 4;
+            this.buttons.share.y = this.height / 2;
+            this.addChild(this.buttons.share);
+            this.buttons.rank.x = this.width / 4 * 2;
             this.buttons.rank.y = this.height / 2;
             this.addChild(this.buttons.rank);
-            this.buttons.start.x = this.width / 3 * 2;
+            this.buttons.start.x = this.width / 4 * 3;
             this.buttons.start.y = this.height / 2;
             this.addChild(this.buttons.start)
         }
@@ -44,6 +48,7 @@ namespace ButtonSet {
             super();
             this.touchEnabled = true;
             this.anchorOffsetX = this.anchorOffsetY = 60;
+            this.scaleX = this.scaleY = 0.8;
             switch (type) {
                 case Button.START:
                     this.drawStart();
@@ -52,7 +57,7 @@ namespace ButtonSet {
                     this.drawRank();
                     break;
                 case Button.SHARE:
-                    // this.drawShare();
+                    this.drawShare();
                     break;
                 default:
                     throw new Error("");
@@ -65,12 +70,15 @@ namespace ButtonSet {
             bg.graphics.drawRect(0, 0, 120, 120);
             bg.graphics.endFill();
             this.addChild(bg);
+
             this.graphics.lineStyle(3, 0xffffff);
             this.graphics.drawCircle(60, 60, 60);
+
             this.graphics.moveTo(105, 60);
             this.graphics.lineTo(37.5, 18.75);
             this.graphics.lineTo(37.5, 101.25);
             this.graphics.lineTo(105, 60);
+
             this.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
                 Main.getInstance().startTask();
             }, this);
@@ -82,21 +90,49 @@ namespace ButtonSet {
             bg.graphics.drawRect(0, 0, 120, 120);
             bg.graphics.endFill();
             this.addChild(bg);
+
             this.graphics.lineStyle(3, 0xffffff);
             this.graphics.drawCircle(60, 60, 60);
+
             this.graphics.moveTo(93.75, 97.5);
             this.graphics.lineTo(26.25, 97.5);
             this.graphics.lineTo(26.25, 45);
             this.graphics.lineTo(48.75, 45);
-            this.graphics.moveTo(48.75, 97.5);
+            this.graphics.lineTo(48.75, 97.5);
             this.graphics.lineTo(48.75, 22.5);
             this.graphics.lineTo(71.25, 22.7);
             this.graphics.lineTo(71.25, 97.5);
             this.graphics.lineTo(71.25, 60);
             this.graphics.lineTo(93.75, 60);
             this.graphics.lineTo(93.75, 97.5);
+
             this.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
                 //TODO: 排行榜
+            }, this);
+        }
+
+        protected drawShare() {
+            let bg = new egret.Shape();
+            bg.graphics.beginFill(0x000000, 0);
+            bg.graphics.drawRect(0, 0, 120, 120);
+            bg.graphics.endFill();
+            this.addChild(bg);
+
+            this.graphics.lineStyle(3, 0xffffff);
+            this.graphics.drawCircle(60, 60, 60);
+
+            this.graphics.lineStyle(10, 0xffffff);
+            this.graphics.drawCircle(75, 30, 5);
+            this.graphics.drawCircle(22.5, 60, 5);
+            this.graphics.drawCircle(75, 90, 5);
+
+            this.graphics.lineStyle(5, 0xffffff);
+            this.graphics.moveTo(75, 30);
+            this.graphics.lineTo(22.5, 60);
+            this.graphics.lineTo(75, 90);
+
+            this.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
+                //TODO: 分享
             }, this);
         }
     }
