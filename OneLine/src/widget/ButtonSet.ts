3525fc5f1b22bcb8ed38d1ddebc885101eca2d5b
chenyingzhou <echocyz@gmail.com>
2018-09-12 14:40:52 +0800

    更新ButtonSet

--- OneLine/src/widget/ButtonSet.ts
+++ OneLine/src/widget/ButtonSet.ts
@@ -2,7 +2,8 @@ namespace ButtonSet {
     export class ButtonSet extends egret.DisplayObjectContainer {
         protected static instance: ButtonSet;
         protected buttons = {
-            "start": new Button(Button.START)
+            "start": new Button(Button.START),
+            "rank": new Button(Button.RANK),
         };
 
         public static getInstance(): ButtonSet {
@@ -12,20 +13,23 @@ namespace ButtonSet {
             return ButtonSet.instance;
         }
 
-        public constructor() {
+        protected constructor() {
             super();
             this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
         }
 
         protected onAddToStage(e: egret.Event) {
-            this.width = this.stage.stageWidth - 130;
-            this.height = 80;
-            let bg = new egret.Shape();
-            bg.graphics.beginFill(0x000000, 1);
-            bg.graphics.drawRect(0, 0, this.width, this.height);
-            bg.graphics.endFill();
-            this.addChild(bg);
-            this.buttons.start.x = this.width / 2;
+            this.width = this.stage.stageWidth;
+            this.height = 120;
+            this.loadCommon();
+        }
+
+        public loadCommon() {
+            this.scaleX = this.scaleY = 0.8;
+            this.buttons.rank.x = this.width / 3;
+            this.buttons.rank.y = this.height / 2;
+            this.addChild(this.buttons.rank);
+            this.buttons.start.x = this.width / 3 * 2;
             this.buttons.start.y = this.height / 2;
             this.addChild(this.buttons.start)
         }
@@ -33,14 +37,22 @@ namespace ButtonSet {
 
     class Button extends egret.Sprite {
         public static START: string = "start";
+        public static RANK: string = "rank";
+        public static SHARE: string = "share";
 
         public constructor(type: string) {
             super();
             this.touchEnabled = true;
-            this.anchorOffsetX = this.anchorOffsetY = 40;
+            this.anchorOffsetX = this.anchorOffsetY = 60;
             switch (type) {
                 case Button.START:
                     this.drawStart();
+                    break;
+                case Button.RANK:
+                    this.drawRank();
+                    break;
+                case Button.SHARE:
+                    // this.drawShare();
                     break;
                 default:
                     throw new Error("");
@@ -50,21 +62,42 @@ namespace ButtonSet {
         protected drawStart() {
             let bg = new egret.Shape();
             bg.graphics.beginFill(0x000000, 0);
-            bg.graphics.drawRect(0, 0, 80, 80);
+            bg.graphics.drawRect(0, 0, 120, 120);
             bg.graphics.endFill();
             this.addChild(bg);
-            this.graphics.lineStyle(2, 0xffffff);
-            this.graphics.drawCircle(40, 40, 40);
-            this.graphics.moveTo(60, 40);
-            this.graphics.lineTo(30, 26);
-            this.graphics.lineTo(30, 54);
-            this.graphics.lineTo(60, 40);
-            this.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onStart, this);
+            this.graphics.lineStyle(3, 0xffffff);
+            this.graphics.drawCircle(60, 60, 60);
+            this.graphics.moveTo(105, 60);
+            this.graphics.lineTo(37.5, 18.75);
+            this.graphics.lineTo(37.5, 101.25);
+            this.graphics.lineTo(105, 60);
+            this.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
+                Main.getInstance().startTask();
+            }, this);
         }
 
-        protected onStart(e: egret.TouchEvent) {
-            this.touchEnabled = false;
-            Main.getInstance().startTask();
+        protected drawRank() {
+            let bg = new egret.Shape();
+            bg.graphics.beginFill(0x000000, 0);
+            bg.graphics.drawRect(0, 0, 120, 120);
+            bg.graphics.endFill();
+            this.addChild(bg);
+            this.graphics.lineStyle(3, 0xffffff);
+            this.graphics.drawCircle(60, 60, 60);
+            this.graphics.moveTo(93.75, 97.5);
+            this.graphics.lineTo(26.25, 97.5);
+            this.graphics.lineTo(26.25, 45);
+            this.graphics.lineTo(48.75, 45);
+            this.graphics.moveTo(48.75, 97.5);
+            this.graphics.lineTo(48.75, 22.5);
+            this.graphics.lineTo(71.25, 22.7);
+            this.graphics.lineTo(71.25, 97.5);
+            this.graphics.lineTo(71.25, 60);
+            this.graphics.lineTo(93.75, 60);
+            this.graphics.lineTo(93.75, 97.5);
+            this.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
+                //TODO: 排行榜
+            }, this);
         }
     }
 }
